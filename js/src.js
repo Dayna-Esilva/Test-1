e0a4ca9727dafc91753f7d93900a3d9832157661
Dayna <81248076+Dayna-Esilva@users.noreply.github.com>
2021-07-05 14:37:10 +0200

    Add files via upload

--- js/src.js
+++ js/src.js
@@ -1,6 +1,6 @@
 
 function myFunction() {
-    var x = document.getElementById("myTopnav");
+    let x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
@@ -9,4 +9,19 @@ function myFunction() {
     console.log(x)
   }
 
-  
+
+  function displayComment(){
+    /* Here we just retrieve the text box value. Note it just overwrites and does not append 
+     so you could just set some static comments added here so that it looks like some comments already exist
+     and then append the new comment
+    */
+    
+    /* Gets the textbov value.... */
+    let blogComment = document.getElementById('txtComment').value;
+    /* If comment had been saved to a text file then the javascript to read the text file should go here... still working on it
+    */
+    
+    document.getElementById('myblgComments').value = blogComment;
+    
+
+}
